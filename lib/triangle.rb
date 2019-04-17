1ba0e11c968236662ba4b562eaa7eedab842dbf7
kathinka martinsen <kathinkamartinsen@gmail.com>
2019-04-17 19:05:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,20 @@
 class Triangle
-  # write code here
+  def initialize (x,y,z)
+    @x = x
+    @y = y
+    @z = z
+
+    # validate
+    #kind
+  end
+
+  def validate
+    if x <= 0 || y <= 0 || z <= 0
+      TriangleError
+  end
+
+
+  def kind
+    
+  end
 end
