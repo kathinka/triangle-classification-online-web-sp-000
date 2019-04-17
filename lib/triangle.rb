bc815208c9f6b2de4a88cb92e1ea4bc0ae446008
kathinka martinsen <kathinkamartinsen@gmail.com>
2019-04-17 19:07:47 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,10 +11,17 @@ class Triangle
   def validate
     if x <= 0 || y <= 0 || z <= 0
       TriangleError
+    elsif x+y < z || x+z <y || y+z < x
+      TriangleError  
   end
 
 
   def kind
 
   end
+
+  class TriangleError < StandardError
+    # triangle error code
+  end
+  
 end
