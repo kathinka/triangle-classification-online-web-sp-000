cb8b9f927c99817aeb9840dcb3ec9a838738ce12
kathinka martinsen <kathinkamartinsen@gmail.com>
2019-04-17 19:25:26 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,34 +1,5 @@
 
 class Triangle
-attr_reader :x, :y, :z
 
-  def initialize(x,y,z)
-    @x = x
-    @y = y
-    @z = z
-  end
-
-  def kind
-      validate
-      if x == y && y == z
-        :equilateral
-      elsif x == y || y == z || z == x
-        :isosceles
-      else
-        :scalene
-      end
-  end
-
-  def validate
-    if x <= 0 || y <= 0 || z <= 0
-      TriangleError
-    elsif x+y < z || x+z <y || y+z < x
-      TriangleError
-    end
-  end
-
-  class TriangleError < StandardError
-    # triangle error code
-  end
 
 end
